098d334e5491b5b0e5c744cec0045665f4661f28
suifengbaobao <suifengzjiabao@163.com>
2018-04-14 18:12:18 +0800

    优化代码

--- impl/src/main/java/cn/edu/nefu/embedded/user/impl/UserServiceImpl.java
+++ impl/src/main/java/cn/edu/nefu/embedded/user/impl/UserServiceImpl.java
@@ -24,7 +24,7 @@ public class UserServiceImpl implements UserService {
   }
 
   @Override
-  public RemoteResult<Long> deleteByUserId(Long userId) {
+  public RemoteResult<Boolean> deleteByUserId(Long userId) {
     return null;
   }
 
